1098c3e43fa0b80612a76fd3ddf45a506b583a50
Emba Moussa <embasbm@gmail.com>
2023-10-17 21:01:00 +0200

    chore: configure devise routes and authetication for admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
   resources :products, only: :index
+
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root "products#index"
 end
