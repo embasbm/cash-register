befddbe7fba2b8431c1d1f2c16f8d93e6baff462
Emba Moussa <embasbm@gmail.com>
2023-10-18 12:39:14 +0200

    feat: force system user to login first when accessing products index page
    And allow login logout, signup actions by providing link to do so.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < InheritedResources::Base
+  before_action :authenticate_user!
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :products, only: :index
-
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  resources :products, only: :index
 
   root "products#index"
 end
