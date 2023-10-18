c744630b467b0e3a8fea4706464672c5f1147a31
Emba Moussa <embasbm@gmail.com>
2023-10-18 16:35:00 +0200

    feat: callback on save for cart to calculate the total_price according to product and quantity on each line

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,9 +1,14 @@
 class Cart < ApplicationRecord
-  has_many :line_items
   belongs_to :user
+  has_many :line_items
 
   monetize :total_price, as: "total_amount"
 
-  validates_numericality_of :total_price,
-    greater_than_or_equal_to: 1, allow_nil: false
+  before_save :calculate_total_price
+
+  private
+
+  def calculate_total_price
+    self.total_price = line_items.sum { |line_item| line_item.product.price_cents * line_item.quantity }
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Cart, type: :model do
 
   it 'will have total_amount column as Money class' do
     expect(cart.total_amount.class).to eq Money
-    expect(cart.total_amount.to_s).to eq '1.30'
+    expect(cart.total_amount.to_s).to eq '0.00'
   end
 
   context 'when total_amount is not integer' do
@@ -25,8 +25,26 @@ RSpec.describe Cart, type: :model do
     end
   end
 
-  it 'belongs to a user' do
-    cart = Cart.reflect_on_association(:user)
-    expect(cart.macro).to eq(:belongs_to)
+  describe "associations" do
+    it 'belongs to a user' do
+      cart = Cart.reflect_on_association(:user)
+      expect(cart.macro).to eq(:belongs_to)
+    end
+
+    it 'should have many line_items' do
+      cart = Cart.reflect_on_association(:line_items)
+      expect(cart.macro).to eq(:has_many)
+    end
+  end
+
+  describe "before_create callback" do
+    let!(:product) { create(:product) }
+    let!(:line_item) { create(:line_item, product: product, cart: cart, quantity: 2) }
+
+    it 'calculates the total price correctly' do
+      cart.line_items << line_item
+      cart.save
+      expect(cart.reload.total_price).to eq(2*product.price_cents)
+    end
   end
 end
