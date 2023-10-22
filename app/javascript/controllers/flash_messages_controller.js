118fb56eca91fe61b712586359c9b04219c978d7
Emba Moussa <embasbm@gmail.com>
2023-10-22 19:58:29 +0200

    feat: applyi styling and make sure flash messages disappear after 5 seconds

--- app/javascript/controllers/flash_messages_controller.js
+++ app/javascript/controllers/flash_messages_controller.js
@@ -1,17 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="flash-messages"
 export default class extends Controller {
   connect() {
-    this.hideMessagesAfterDelay()
-  }
-
-  hideMessagesAfterDelay() {
-    const frames = document.querySelectorAll("[data-turbo-frame='flash-frame']")
-    frames.forEach((frame) => {
-      setTimeout(() => {
-        frame.parentNode.removeChild(frame)
-      }, 5000) // Adjust the delay (in milliseconds) as needed
-    })
+    setTimeout(() => {
+      this.element.remove();
+    }, 5000);
   }
 }
