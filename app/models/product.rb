486739d58428d18efcfe17b17530aad1ca4c0b7f
Emba Moussa <embasbm@gmail.com>
2023-10-25 00:26:39 +0200

    feat: move basic instance methods to product model
    To add one value or rest/remove

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,16 @@ class Product < ApplicationRecord
   monetize :price_cents
   validates_numericality_of :amount, greater_than_or_equal_to: 0, allow_nil: false
 
+  def remove_stock!
+    self.amount -= 1
+    save!
+  end
+
+  def add_stock!
+    self.amount += 1
+    save!
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["code", "created_at", "id", "name", "price_cents", "updated_at", "amount"]
   end
